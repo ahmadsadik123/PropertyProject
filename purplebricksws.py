ce4504be78d713c76f503487bcfa2800e00d3d81
Ahmad Sadik <124929431+ahmadsadik123@users.noreply.github.com>
2023-03-23 16:36:53 +0000

    commit

--- purplebricksws.py
+++ purplebricksws.py
@@ -1,14 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
 from selenium.common.exceptions import NoSuchElementException
-from datetime import datetime
 
 
 options = Options()
